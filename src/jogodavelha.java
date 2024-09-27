4dcc91a580577725d80911479202558c778262a6
braiandaros <braianjunior666@gmail.com>
2024-09-27 16:30:36 -0300

    ajuste de jogo

--- src/jogodavelha.java
+++ src/jogodavelha.java
@@ -8,17 +8,25 @@ public class jogodavelha {
 	   public static void main(String[] args) {
 	      Scanner scanner = new Scanner(System.in);
 	      char[][] tabuleiro = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-	      char player = 'X' ;
-	      int linha, coluna;
-
+	      String player = "a";
+	      char ponto;
+	      int linha, coluna, contador = 0;
+	      
+	      System.out.println("Digite o nome do jogador 1: ");
+	      String nomep1 = scanner.nextLine();
+	      System.out.println("Digite o nome do jogador 2: ");
+	      String nomep2 = scanner.nextLine();
+	      player = nomep1;
+	      
 	      while(true) {
-	         System.out.println("  0  1  2");
-	         System.out.println("0 "+tabuleiro[0][0]+" |"+tabuleiro[0][1]+" | "+tabuleiro[0][2]);
-	         System.out.println("  --+--+--");
-	         System.out.println("1 "+tabuleiro[1][0]+" |"+tabuleiro[1][1]+" | "+tabuleiro[1][2]);
-	         System.out.println("  --+--+--");
-	         System.out.println("2 "+tabuleiro[2][0]+" |"+tabuleiro[2][1]+" | "+tabuleiro[2][2]);
+	         System.out.println("  0    1   2");
+	         System.out.println("0 "+tabuleiro[0][0]+"  | "+tabuleiro[0][1]+" |  "+tabuleiro[0][2]);
+	         System.out.println("  ---+---+---");
+	         System.out.println("1 "+tabuleiro[1][0]+"  | "+tabuleiro[1][1]+" |  "+tabuleiro[1][2]);
+	         System.out.println("  ---+---+---");
+	         System.out.println("2 "+tabuleiro[2][0]+"  | "+tabuleiro[2][1]+" |  "+tabuleiro[2][2]);
 
+	         
 	         System.out.println("Vez do jogador "+player+".");
 	         System.out.print("Digite a linha: ");
 	         linha = scanner.nextInt();
@@ -30,17 +38,24 @@ public class jogodavelha {
 	            continue;
 	         }
 
-	         tabuleiro[linha][coluna] = player;
-
-	         if(tabuleiro[0][0] == player && tabuleiro[0][1] == player && tabuleiro[0][2] == player ||
-	        	tabuleiro[1][0] == player && tabuleiro[1][1] == player && tabuleiro[1][2] == player ||
-	        	tabuleiro[2][0] == player && tabuleiro[2][1] == player && tabuleiro[2][2] == player ||
-	        	tabuleiro[0][0] == player && tabuleiro[1][0] == player && tabuleiro[2][0] == player ||
-	        	tabuleiro[0][1] == player && tabuleiro[1][1] == player && tabuleiro[2][1] == player ||
-	        	tabuleiro[0][2] == player && tabuleiro[1][2] == player && tabuleiro[2][2] == player ||
-	        	tabuleiro[0][0] == player && tabuleiro[1][1] == player && tabuleiro[2][2] == player ||
-	        	tabuleiro[0][2] == player && tabuleiro[1][1] == player && tabuleiro[2][0] == player) {
+	         if(player == nomep1) {
+	        	 ponto = 'X';
+	        	 tabuleiro[linha][coluna] = ponto;
 	        	 
+	         }else {
+	        	ponto = 'O';
+	        	tabuleiro[linha][coluna] = ponto;
+	         }
+	         if(tabuleiro[0][0] == ponto && tabuleiro[0][1] == ponto && tabuleiro[0][2] == ponto ||
+	        	tabuleiro[1][0] == ponto && tabuleiro[1][1] == ponto && tabuleiro[1][2] == ponto ||
+	        	tabuleiro[2][0] == ponto && tabuleiro[2][1] == ponto && tabuleiro[2][2] == ponto ||
+	        	tabuleiro[0][0] == ponto && tabuleiro[1][0] == ponto && tabuleiro[2][0] == ponto ||
+	        	tabuleiro[0][1] == ponto && tabuleiro[1][1] == ponto && tabuleiro[2][1] == ponto ||
+	        	tabuleiro[0][2] == ponto && tabuleiro[1][2] == ponto && tabuleiro[2][2] == ponto ||
+	        	tabuleiro[0][0] == ponto && tabuleiro[1][1] == ponto && tabuleiro[2][2] == ponto ||
+	        	tabuleiro[0][2] == ponto && tabuleiro[1][1] == ponto && tabuleiro[2][0] == ponto) {
+	        	 
+	        	System.out.println("--------------------------");
 	            System.out.println("Jogador "+player+" venceu!");
 	            
 		        System.out.println("  0  1  2");
@@ -49,14 +64,19 @@ public class jogodavelha {
 		        System.out.println("1 "+tabuleiro[1][0]+" |"+tabuleiro[1][1]+" | "+tabuleiro[1][2]);
 		        System.out.println("  --+--+--");
 		        System.out.println("2 "+tabuleiro[2][0]+" |"+tabuleiro[2][1]+" | "+tabuleiro[2][2]);
+		        System.out.println("--------------------------");
+		        
 	            break;
+	         
 	         }
-
-	         if(player == 'X') {
-	            player = 'O';
+	         
+	         if(contador%2 == 0) {
+	            player = nomep2;
 	         } else {
-	            player = 'X';
+	            player = nomep1;
 	         }
+	         contador += 1;
+	         System.out.println(contador);
 	      }
 
 	      scanner.close();
